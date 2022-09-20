e477ef9923c2639e630831b038b179cb15d6c883
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-09-19 21:52:37 -0300

    SQL final

--- testAugen.sql
+++ testAugen.sql
@@ -1,38 +1,43 @@
-create schema testAugen;
-use testAugen;
+create schema augen;
+use augen;
 
 create table equipments (
-	id int auto_increment,
-	name varchar(30) not null,
-	city_id int not null,
-	PRIMARY KEY(id)
-);
-
-create table cities (
-	id int auto_increment, 
-	name varchar(30) not null,
-    state varchar(30) not null,
-	PRIMARY KEY(id)
-);
-
-create table analysis (
-	id int auto_increment,
-	ph float not null,
-	chlorine float not null,
+    id int auto_increment,
+    name varchar(30) not null,
+    city_id int not null,
+    created_at datetime default CURRENT_TIMESTAMP,
+    deleted_at datetime null,
+    PRIMARY KEY(id)
+  );
+  
+  create table cities (
+    id int auto_increment, 
+    name varchar(30) not null,
+      state varchar(30) not null,
+    PRIMARY KEY(id)
+  );
+  
+  create table analysis (
+    id int auto_increment,
+    ph float not null,
+    chlorine float not null,
     fluorine float not null,
     output float not null,
     equipment_id int not null,
-	PRIMARY KEY(id)
-);
-
-create table user (
-	id int auto_increment, 
-	email varchar(30) not null,
-    password char(200) not null,
+    created_at datetime default CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
-);
-
-alter table analysis add foreign key (equipment_id) references equipments(id);
-alter table equipments add foreign key (city_id) references cities(id);
+  );
+  
+  create table user (
+    id int auto_increment, 
+    email varchar(30) not null,
+    password char(200) not null,
+    created_at datetime default CURRENT_TIMESTAMP,
+      PRIMARY KEY(id)
+  );
+  
+  alter table analysis add foreign key (equipment_id) references equipments(id);
+  alter table equipments add foreign key (city_id) references cities(id);
+  
 
 
